a43df52cf0c41077b6c08f2a92528b3c62df3773
tottorisnow33 <tottorisnow33@outlook.com>
2022-12-29 13:06:35 +0900

    コード上からビューを配置するように変更

--- BingoGosyuin2/app/src/main/java/com/makty/bingogosyuin/MainActivity.java
+++ BingoGosyuin2/app/src/main/java/com/makty/bingogosyuin/MainActivity.java
@@ -1,10 +1,17 @@
 package com.makty.bingogosyuin;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,9 +22,11 @@ import com.makty.bingogosyuin.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Context m_context = null;       /*コンテキスト*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -32,6 +41,33 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        Log.v("aa", "nekoMainActivity Created");
+
+//        m_context = this.getBaseContext();
+//
+//        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.navigation_home);
+//
+//        m_context = fragment.getActivity();
+//
+//        LinearLayout linearLayout = new LinearLayout(m_context);
+//        setContentView(linearLayout);
+//
+//        ViewGroup.LayoutParams layoutParams  = linearLayout.getLayoutParams();
+//        layoutParams.height = 200;
+//        layoutParams.width = 200;
+//
+//        linearLayout.setLayoutParams(layoutParams);
+
+
+//        m_context. (linearLayout);
+        /*ボタン作成*/
+//        Button button1 = new Button(this);
+//        button1.setText("Button1");
+//        linearLayout.addView(button1);
+
+
+
     }
 
 }
--- BingoGosyuin2/app/src/main/java/com/makty/bingogosyuin/ui/home/HomeFragment.java
+++ BingoGosyuin2/app/src/main/java/com/makty/bingogosyuin/ui/home/HomeFragment.java
@@ -1,20 +1,40 @@
 package com.makty.bingogosyuin.ui.home;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.makty.bingogosyuin.R;
 import com.makty.bingogosyuin.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private Context m_context = null;       /*コンテキスト*/
+    private View m_view = null;
+
+    private boolean clicked = false;
+    private @ColorInt
+    int mBackgroundColor = Color.TRANSPARENT;
+
+    /**画面上のビュー**/
+    ScrollView m_scrollView = null;
+
+    Button mButton0_0 = null;
+    Button mButton0_1 = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,11 +43,40 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        m_context = this.getContext();
 
-        //final TextView textView = binding.textHome;
-        //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        /*バックグラウンドのスクロールビュー取得*/
+        m_scrollView = (ScrollView) root.findViewById(R.id.scrollViewHome);
+
+        /*ボタン設置レイアウト作成*/
+        LinearLayout layout = new LinearLayout(m_context);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+
+        layout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        layout.setGravity(Gravity.CENTER);
+
+        /*ボタン作成*/
+        mButton0_0 = new Button(m_context);
+        mButton0_0.setBackgroundColor(0xFFee7800);
+        mButton0_0.setText("鹿島神社\n(茨城県)");
+        layout.addView(mButton0_0);
+
+        mButton0_1 = new Button(m_context);
+        mButton0_1.setBackgroundColor(0xFFee7800);
+        mButton0_1.setGravity(Gravity.CENTER);
+        mButton0_1.setText("鹿島神社\n(茨城県)");
+        layout.addView(mButton0_1);
+
+        /*スクロールビューにレイアウトを追加*/
+        m_scrollView.addView(layout);
+
         return root;
     }
+
+
 
     @Override
     public void onDestroyView() {
